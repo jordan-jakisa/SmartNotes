1cdb85a1ac217aedc928a0a897bb5b9aa702cf0f
Jordan <jordan.jakisa@gmail.com>
2023-02-27 17:31:52 +0300

    Django admin user management

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home', views.home)
+    path('home', views.home),
+    path('authorized', views.authorized)
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+# requires authentication to access the page
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
 
