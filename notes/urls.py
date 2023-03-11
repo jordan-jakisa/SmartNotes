093a330a876e1f2935be810ae943de0684533267
Jordan <jordan.jakisa@gmail.com>
2023-03-11 23:47:45 +0300

    Styling using Bootstrap and dynamically generating links

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NotesListView.as_view()),
-    path('notes/<int:pk>', views.NotesDetailView.as_view()),
+    path('notes', views.NotesListView.as_view(), name="notes.list"),
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
 ]
--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'smartnotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR, 'static/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
