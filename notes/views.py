326be8748531fbec039a12ef0f8acf1b37975917
Jordan <jordan.jakisa@gmail.com>
2023-03-15 22:23:53 +0300

    Update and Delete CRUD operations

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,16 @@
 from .models import Notes
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from .forms import NotesForm
+from django.views.generic.edit import DeleteView
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesCreateView(CreateView):
     model = Notes
